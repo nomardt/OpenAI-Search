c31657a85ca24466f59cc4d210e558249fe16369
Nomardt <115152912+nomardt@users.noreply.github.com>
2023-01-15 08:33:35 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -13,5 +13,8 @@ mv main.py /opt/openai-search
 printf "\nI don't know how to make a proper installer, so "
 printf "you'll have add the following to your .bashrc file:"
 printf "\nalias ais='/opt/openai-search/main.py'\n"
+printf "Also don't forget to type 'source .bashrc' to apply changes.\n"
+printf "\nSometimes it might take a while for OpenAI's servers to process "
+printf "your query, so don't panic if you don't get a response immediately :)\n"
 
 rm -r ../OpenAI-Search-main
