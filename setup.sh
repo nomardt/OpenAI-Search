15f47122c19563fc4e24146cc59ef488e0d3c23f
Ivanich41 <cooldude412003@gmail.com>
2023-01-15 13:31:32 +0300

    Setup script enhance

--- setup.sh
+++ setup.sh
@@ -7,12 +7,19 @@ read api_key
 sed -i "s/ENTER_YOUR_API/${api_key}/" ./main.py
 chmod +x main.py
 
-mkdir /opt/openai-search
-mv main.py /opt/openai-search
+sudo mkdir /opt/openai-search && mv main.py /opt/openai-search
 
-printf "\nI don't know how to make a proper installer, so "
-printf "you'll have add the following to your .bashrc file:"
+echo "alias ais='/opt/openai-search/main.py" >> ~/.bashrc
+source ~/.bashrc 
+if [ -f "/usr/bin/zsh" ]; then
+	echo "alias ais='/opt/openai-search/main.py" >> ~/.zshrc
+	source ~/.zshrc 
+fi
+
+printf "\nThis will work for bash and zsh shells for curent user"
+printf "\nIf you want to add this command for other users or your another shell add"
 printf "\nalias ais='/opt/openai-search/main.py'\n"
+printf "\n to .rc file of selected shell."
 printf "Also don't forget to type 'source .bashrc' to apply changes.\n"
 printf "\nSometimes it might take a while for OpenAI's servers to process "
 printf "your query, so don't panic if you don't get a response immediately :)\n"
