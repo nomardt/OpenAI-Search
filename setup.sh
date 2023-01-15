b2bb9c1cb14741b9ce40561aad0ba90ed2638f29
Ivanich41 <cooldude412003@gmail.com>
2023-01-15 13:33:12 +0300

    Forgot sudo

--- setup.sh
+++ setup.sh
@@ -7,21 +7,22 @@ read api_key
 sed -i "s/ENTER_YOUR_API/${api_key}/" ./main.py
 chmod +x main.py
 
-sudo mkdir /opt/openai-search && mv main.py /opt/openai-search
+sudo mkdir /opt/openai-search 
+sudo cp main.py /opt/openai-search
 
-echo "alias ais='/opt/openai-search/main.py" >> ~/.bashrc
-source ~/.bashrc 
+alias ais="/opt/openai-search/main.py"
+#source ~/.bashrc 
 if [ -f "/usr/bin/zsh" ]; then
-	echo "alias ais='/opt/openai-search/main.py" >> ~/.zshrc
-	source ~/.zshrc 
+ 	alias ais="/opt/openai-search/main.py"
+	#source ~/.zshrc 
 fi
 
 printf "\nThis will work for bash and zsh shells for curent user"
-printf "\nIf you want to add this command for other users or your another shell add"
+printf "\nIf you want to add this command for other users or your another shell add\n"
 printf "\nalias ais='/opt/openai-search/main.py'\n"
-printf "\n to .rc file of selected shell."
-printf "Also don't forget to type 'source .bashrc' to apply changes.\n"
+printf "\nto .rc file of selected shell."
+printf "\nAlso don't forget to type 'source ~/.bashrc' (or source ~/.zshrc) to apply changes.\n"
 printf "\nSometimes it might take a while for OpenAI's servers to process "
 printf "your query, so don't panic if you don't get a response immediately :)\n"
 
-rm -r ../OpenAI-Search-main
+rm -r ../OpenAI-Search-main # What is it? :D
