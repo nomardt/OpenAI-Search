a20128a1c726b52ce2693169a9dccb82305d90ef
LagrangeH <maxsergeev39@gmail.com>
2023-01-16 07:30:33 +0300

    Fix image mode feature and add .env parsing

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 import sys
 
 import argparse
+from dotenv import dotenv_values
 import openai
 
 
@@ -17,7 +18,7 @@ def parse_args() -> argparse.Namespace:
         default=0.2,
         type=float,
         required=False,
-        help="The temperature determines how greedy the generative model is.",
+        help="When -i flag is passed. The temperature determines how greedy the generative model is.",
         metavar='temperature',
         dest='temp',
     )
@@ -33,20 +34,20 @@ def parse_args() -> argparse.Namespace:
 
     parser.add_argument(
         '-i', '--generate_image',
-        type=str,
-        required=False,
-        help='Change the mode to image generation.',
-        metavar='generate_image',
-        dest='generate_image'
+        action='store_true',
+        help='Generate image by prompt.',
+        dest='img_request'
     )
 
     parser.add_argument(
         '-n',
+        nargs=1,
         type=int,
         required=False,
-        help='WORKS ONLY WITH -i; Specify the number of images to be generated',
-        metavar='n',
-        dest='n'
+        default=1,
+        help='When -i flag is passed; Specify the number of images to be generated (from 1 to 10, default 1)',
+        metavar='images number',
+        dest='img_number'
     )
 
     parser.add_argument(
@@ -60,7 +61,8 @@ def parse_args() -> argparse.Namespace:
     return parser.parse_args()
 
 
-def ai_request(prompt: str, temp: float) -> str:
+# TODO: create class
+def ai_request_text(prompt: str, temp: float) -> str:
     return openai.Completion.create(
         engine="text-davinci-003",
         prompt=prompt,
@@ -68,31 +70,35 @@ def ai_request(prompt: str, temp: float) -> str:
         temperature=temp
     )["choices"][0]["text"]
 
+
 def ai_request_image(prompt: str, n: int) -> str:
-    return openai.Completion.create(
+    return openai.Image.create(
         prompt=prompt,
         n=n,
         size='1024x1024'
     )["data"][0]["url"]
 
+
 def main():
     config = parse_args()
+    # TODO: add writing api_key to .env
 
-    if generate_image:
-        if not n:
-            n = 1
+    openai.api_key = dotenv_values('.env')['API_KEY']
 
-        print(ai_request_image(generate_image, n))
+    config.prompt = ' '.join(config.prompt)
+
+    if config.img_request:
+        print("[Query | Image]", config.prompt)
+        # TODO: add link shorter
+        print("[AI | URL]", ai_request_image(config.prompt, config.img_number))
 
     else:
         if not (0. <= config.temp <= 1.):
             config.temp = 0.2
             print("The temperature only accepts floating point numbers from 0 to 1. Value 0.2 specified instead.")
 
-        config.prompt = ' '.join(config.prompt)
-
         print("[Query]", config.prompt)
-        print("[AI]", ai_request(config.prompt, config.temp))
+        print("[AI]", ai_request_text(config.prompt, config.temp))
 
 
 if __name__ == '__main__':
