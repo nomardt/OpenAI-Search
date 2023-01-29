0cb517d5761ff988027ccaf8abb8b3e1b5dc572d
Nomardt <115152912+nomardt@users.noreply.github.com>
2023-01-29 03:07:40 +0000

    True fix

--- setup.sh
+++ setup.sh
@@ -5,23 +5,25 @@ pip install -r requirements.txt
 gzip docs/ais.1
 sudo cp docs/ais.1.gz /usr/share/man/man1
 
-sudo cp -r ./ opt/
+sudo mkdir /opt/openai-search 
 
 printf "\nPlease enter your OpenAI API key:\n"
-# shellcheck disable=SC2162
 read api_key
-sudo touch /opt/openai-search/src/.env
-sudo chmod 666 /opt/openai-search/src/.env
-echo API_KEY=\'"$api_key"\' > /opt/openai-search/src/.env
+sudo touch /opt/openai-search/.env
+sudo chmod 666 /opt/openai-search/.env
+echo API_KEY=\'$api_key\' > /opt/openai-search/.env
 
-chmod +x opt/openai-search/src/main.py
+chmod +x main.py
+sudo cp src/main.py /opt/openai-search
 
-echo 'alias ais="/opt/openai-search/src/main.py"' >> ~/.bashrc
+echo 'alias ais="/opt/openai-search/main.py"' >> ~/.bashrc
 if [ -f "/usr/bin/zsh" ]; then
- 	echo 'alias ais="/opt/openai-search/src/main.py"' >> ~/.zshrc
+ 	echo 'alias ais="/opt/openai-search/main.py"' >> ~/.zshrc
 fi
 
 printf "\nInstallation finished successfully!\nThe program is installed to /opt/openai-search and we've added an alias to your bashrc (or zshrc)."
 printf "\nBefore using the program please end the current session or type:\n'source ~/.bashrc' - for bash; 'source ~/.zshrc' - for zsh\n"
 printf "To view the manual, type: 'man ais'\n"
 printf "\nSometimes it might take a while for OpenAI's servers to process your query, so don't panic if you don't get a response immediately :)\nEnjoy!\n"
+
+rm -rf ../OpenAI-Search-main
