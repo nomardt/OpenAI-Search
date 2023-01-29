b0f2a724d07399b10f5642e5b4dc3bc83331d6fe
Nomardt <115152912+nomardt@users.noreply.github.com>
2023-01-29 08:35:36 +0000

    Fixed ALL bugs

--- setup.sh
+++ setup.sh
@@ -6,14 +6,15 @@ if [ -e /opt/openai-search/main.py ]; then
     read response
     if [[ $response == [Yy]es || $response == [Yy] ]]
     then
-        printf "\nUninstalling the previous version...\n"
+        printf "Uninstalling the previous version...\n"
         sudo rm -rf /opt/openai-search
         sudo rm /usr/bin/ais
         sudo rm /usr/share/man/man1/ais.1.gz
         printf "The previous version uninstalled successfully!\nThe installation process will now continue as usual..."
     else
-        printf "\nInstallation interrupted!\n"
+        printf "Installation interrupted!\n"
         exit 0
+    fi
 fi
 
 pip install -r requirements.txt
@@ -29,7 +30,7 @@ sudo touch /opt/openai-search/.env
 sudo chmod 666 /opt/openai-search/.env
 echo API_KEY=\'$api_key\' > /opt/openai-search/.env
 
-chmod +x main.py
+chmod +x src/main.py
 sudo cp src/main.py /opt/openai-search
 
 sudo ln -s /opt/openai-search/main.py /usr/bin/ais
