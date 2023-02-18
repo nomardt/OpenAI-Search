a57630af8f722a0310e1e836b907e4b6d32205d6
LagrangeH <maxsergeev39@gmail.com>
2023-02-19 02:43:32 +0300

    Refactor

--- src/env_loader.py
+++ src/env_loader.py
@@ -1,14 +1,36 @@
 import os
 
-import dotenv
+from environs import Env
 
 
-# Load environment variables from .env file
-dotenv.load_dotenv()
+def load_env():
+    env = Env()
+    env.read_env()
 
-# Accessing environment variables
-API_KEY = os.getenv("API_KEY")
+    return env.str("API_KEY"), env.bool("DEBUG", False)
 
 
-def write_env_var(key, value):
-    dotenv.set_key(dotenv.find_dotenv(), key, value)
+def change_api_key(value):
+    # Read the .env file into a dictionary
+    env_vars = {}
+    with open('.env', 'r') as f:
+        for line in f:
+            if line:
+                key, value = line.split('=')
+                env_vars[key] = value
+k
+    env_vars['API_KEY'] = value
+
+    # Write the updated dictionary back to the .env file
+    with open('.env', 'w') as f:
+        for key, value in env_vars.items():
+            f.write(f'{key}={value}\n')
+
+    return env_vars['API_KEY']
+
+
+if __name__ == '__main__':
+    print(load_env())
+    a = change_api_key('test')
+    print(a)
+    print(load_env())
